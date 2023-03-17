3d816975b34a4577707cc9dc46d91d882ee267b2
Alexandr Antonov <antonovsacha@yandex.ru>
2023-03-17 17:03:39 +0300

    refactor: вынесена бизнес-логика и хранение информации

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -1,43 +1,73 @@
 package ru.yandex.practicum.filmorate.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exceptions.UniqueObjectException;
-import ru.yandex.practicum.filmorate.exceptions.ValidateException;
+import ru.yandex.practicum.filmorate.exceptions.unchecked.ObjectAlreadyExistException;
+import ru.yandex.practicum.filmorate.exceptions.unchecked.ObjectNotFoundException;
+import ru.yandex.practicum.filmorate.exceptions.unchecked.UniqueObjectException;
+import ru.yandex.practicum.filmorate.exceptions.unchecked.ValidateException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.storage.IfilmStorage;
+import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/films")
 @Slf4j
 public class FilmController {
-
-    private HashMap<Integer, Film> films = new HashMap<>();
     private static int countId = 1;
+
+    private final IfilmStorage filmStorage;
+    private final FilmService filmService;
+
+    @Autowired
+    public FilmController(InMemoryFilmStorage inMemoryFilmStorage, FilmService filmService) {
+        this.filmStorage = inMemoryFilmStorage;
+        this.filmService = filmService;
+
+    }
 
     @GetMapping
     public Collection<Film> getFilms() {
-        return films.values();
+        return filmStorage.getValues();
+    }
+
+    @GetMapping("/{id}")
+    public Film getFilmById(@PathVariable(value = "id") Integer id) {
+        if (!filmStorage.containsKey(id)) {
+            throw new ObjectNotFoundException("Object Film id = " + id + " not found");
+        }
+        return filmStorage.get(id);
+    }
+
+    @GetMapping("/popular")
+    public List<Film> getPopularFilms(@RequestParam(required = false, value = "count", defaultValue = "10") Integer count) {
+        if (count < 0) {
+            throw new ValidateException("Variable count is negative or null: " + count);
+        }
+        return filmService.getCountPopularFilms(count);
     }
 
     @PostMapping
     public Film addFilm(@Valid @RequestBody Film film) {
         checkFilmReleaseDate(film);
-        if (films.containsKey(film.getId()) || films.containsValue(film)) {
+        if (filmStorage.contains(film)) {
             log.warn("Повторный запрос на добавление через метод POST {}", film);
             throw new UniqueObjectException("Объект " + film + " уже существует. Воспользуйтесь методом PUT");
         }
-        Film newFilm = film.toBuilder()
-                .id(countId++)
-                .build();
-        films.put(newFilm.getId(), newFilm);
-        log.debug("Добавление объекта методом POST" + film);
-        return newFilm;
-
+        //Убрал билдер, чето бессмысленный он тут
+        film.setId(countId++);
+        filmStorage.put(film);
+        return film;
     }
 
     //Метод PUT в данном случае похоже работает только на замену, так как просто при заносе
@@ -45,13 +75,27 @@ public class FilmController {
     @PutMapping
     public Film replaceFilm(@Valid @RequestBody Film film) {
         checkFilmReleaseDate(film);
-        if (films.containsKey(film.getId())) {
-            films.put(film.getId(), film);
-        } else {
+        if (!filmStorage.containsKey(film.getId())) {
             log.warn("Не существующий объект {} метод PUT", film);
-            throw new ValidateException(film.toString() + "не существует. Воспользуйтесь методом Post");
+            throw new ValidateException(film + "не существует. Воспользуйтесь методом Post");
         }
+        filmStorage.put(film);
         return film;
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    public Integer addLikeToFilm(@PathVariable Integer id, @PathVariable Integer userId) {
+        if (id == null || userId == null) {
+            throw new ValidateException("Отсутствует переменная пути id = " + id + "userId = " + userId);
+        }
+        filmService.addLike(id, userId);
+        return id;
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public Integer deleteLikeFromFilm(@PathVariable Integer id, @PathVariable Integer userId) {
+        filmService.deleteLike(id, userId);
+        return id;
     }
 
     private static boolean checkFilmReleaseDate(Film film) throws ValidateException {
@@ -60,5 +104,15 @@ public class FilmController {
             throw new ValidateException("Дата релиза Film раньше " + borderDate);
         }
         return true;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> handleNotFound(final ObjectNotFoundException e) {
+        return new ResponseEntity<>(Map.of("Exception: ", e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> handleBadRequest(final ObjectAlreadyExistException e) {
+        return new ResponseEntity<>(Map.of("Exception: ", e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -1,58 +1,118 @@
 package ru.yandex.practicum.filmorate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exceptions.UniqueObjectException;
-import ru.yandex.practicum.filmorate.exceptions.ValidateException;
+import ru.yandex.practicum.filmorate.exceptions.unchecked.ObjectAlreadyExistException;
+import ru.yandex.practicum.filmorate.exceptions.unchecked.ObjectNotFoundException;
+import ru.yandex.practicum.filmorate.exceptions.unchecked.ValidateException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.IuserStorage;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
 import java.util.Collection;
-import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    /**
-     * Для UserController:
-     * создание пользователя;
-     * обновление пользователя;
-     * получение списка всех пользователей.
-     */
-    private HashMap<Integer, User> users = new HashMap<>();
     private static int countId = 1;
+
+    private final IuserStorage userStorage;
+    private final UserService userService;
+
+    @Autowired
+    public UserController(InMemoryUserStorage inMemoryUserStorage, UserService userService) {
+        this.userStorage = inMemoryUserStorage;
+        this.userService = userService;
+    }
 
     @GetMapping
     public Collection<User> getUsers() {
-       return users.values();
+       return userStorage.getValues();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable(value = "id") Integer id) {
+        if (!userStorage.containsKey(id)) {
+            throw new ObjectNotFoundException("Объект User c " + id + " not found");
+        }
+        return userStorage.get(id);
+    }
+
+    @GetMapping("/{id}/friends")
+    public Collection<User> getUserFriends(@PathVariable(value = "id") Integer id) {
+        return userService.getFriends(id);
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public Collection<User> getCommonFriends(
+            @PathVariable(value = "id") Integer id,
+            @PathVariable(value = "otherId") Integer otherId
+    ) {
+        return userService.getCommonFriends(id, otherId);
     }
 
     @PostMapping
     public User addUser(@Valid @RequestBody User user) {
-        if (users.containsValue(user) || users.containsKey(user.getId())) {
-            throw new UniqueObjectException("Объект " + user + " уже существует. Воспользуйтесь методом PUT");
+        if (userStorage.contains(user)) {
+            throw new ObjectAlreadyExistException("Объект " + user + " уже существует. Воспользуйтесь методом PUT");
         }
         if (user.getName() == null || user.getName().isEmpty()) {
             user.setName(user.getLogin());
         }
-        User newUser = user.toBuilder()
-                .id(countId++)
-                .build();
-        users.put(newUser.getId(), newUser);
-        return newUser;
+        user.setId(countId++);
+        userStorage.put(user);
+        return user;
     }
+
     //Метод PUT в данном случае похоже работает только на замену, так как просто при заносе
     // в мапу, тесты выдают ошибку
     @PutMapping
     public User replaceUser(@Valid @RequestBody User user) {
-        if (users.containsKey(user.getId())) {
+        if (userStorage.containsKey(user.getId())) {
             if (user.getName() == null || user.getName().isEmpty()) {
                 user.setName(user.getLogin());
             }
-            users.put(user.getId(), user);
+            userStorage.put(user);
         } else {
             throw new ValidateException("Объект " + user + " не найден");
         }
         return user;
+    }
+
+    @PutMapping(value = "/{id}/friends/{friendsId}")
+    public Integer addFriend(
+            @PathVariable(value = "id") Integer id,
+            @PathVariable(value = "friendsId") Integer friendsId
+    ) {
+        if (id == null || friendsId == null) {
+            throw new ValidateException("Отсутствует переменная пути id = " + id + "friendsId = " + friendsId);
+        }
+        userService.addFriend(id, friendsId);
+        return id;
+    }
+
+    @DeleteMapping("/{id}/friends/{friendsId}")
+    public Integer deleteFriend (
+            @PathVariable(value = "id") Integer id,
+            @PathVariable(value = "friendsId") Integer friendsId
+    ) {
+    userService.deleteFriend(id, friendsId);
+    return id;
+    }
+
+    //TODO: переименовать метод
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> handle (final ObjectAlreadyExistException e) {
+        return new ResponseEntity<>(Map.of("Error: ", e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> handleNotFound (final ObjectNotFoundException e) {
+        return new ResponseEntity<>(Map.of("Exception: ", e.getMessage()), HttpStatus.NOT_FOUND);
     }
 }
